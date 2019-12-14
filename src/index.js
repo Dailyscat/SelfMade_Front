3703e537e76dd206d58241d5ad1e07570d92a8a5
Dailyscat <pay_nack@naver.com>
2019-12-14 16:25:38 +0900

    ducks pattern 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { createStore } from "redux";
+import rootReducer from "./store/modules";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
@@ -7,12 +12,19 @@ import { BrowserRouter } from "react-router-dom";
 
 import createHistory from "history/createBrowserHistory";
 
+// 리덕스 개발자도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
 const customHistory = createHistory();
 
 ReactDOM.render(
-  <BrowserRouter history={customHistory}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter history={customHistory}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
